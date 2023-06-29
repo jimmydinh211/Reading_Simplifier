b8a88adbe367bfb811eadba41f32d3bc4d211277
jimmydinh211 <minhdinhtranhoang@gmail.com>
2023-06-30 00:17:56 +0700

    Separate box for confusing words definitions

--- frontend/src/Components/TextBox.js
+++ frontend/src/Components/TextBox.js
@@ -1,11 +1,12 @@
-import React, { useRef, useState, useEffect, useContext } from 'react';
+import React, { useState } from 'react';
 const { Configuration, OpenAIApi } = require("openai");
 
 export default function TextBox() {
     // State for input text
-    const [text, setText] = useState("1");
-    const [rep, setRep] = useState("This is line 1.\nThis is line 2.");
-    const [grade, setSelectedOption] = useState('');
+    const [text, setText] = useState("");
+    const [rep, setRep] = useState("Simplified text...");
+    const [terms, setTerms] = useState([])
+    const [grade, setSelectedOption] = useState('1');
 
 
     // OpenAI configuration and models
@@ -24,11 +25,18 @@ export default function TextBox() {
             const chatCompletion = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo",
                 messages: [
-                    { role: "user", content: `Please change the following text into a version that a ${grade}th grader can understand and put them in bullet points by topics that have proper newline for each bullet points:\n ${text} \n Can you also list the terms from your previous response that might be hard to understand for grade ${grade} students and give those terms their definitions?` }
+                    { role: "user", content: `Please change the following text into a version that a ${grade}th grader can understand and put them in bullet points by topics that have proper newline for each bullet points:\n ${text} \n` },
                 ],
             });
-            console.log(chatCompletion)
             setRep(chatCompletion.data.choices[0].message.content);
+            const termDefiniton = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: [
+                    { role: "user", content: `${chatCompletion.data.choices[0].message.content} \n Can you also list the terms (along with their definitions) from the above excerpt that might be hard to understand for grade ${grade} students in a JSON-formatted response so that I can parse this response and put it in a JSON object in Javascript with the key of each entry being the term and the value being its definition? ` }
+                ]
+            })
+            setTerms(Object.entries(JSON.parse(termDefiniton.data.choices[0].message.content)));
+            console.log(Object.entries(JSON.parse(termDefiniton.data.choices[0].message.content)));
         } catch (error) {
             if (error.response) {
                 console.log(error.response.status);
@@ -54,7 +62,6 @@ export default function TextBox() {
 
         return options;
     };
-    const options = renderOptions();
 
     const handleOptionChange = (event) => {
         setSelectedOption(event.target.value);
@@ -69,21 +76,42 @@ export default function TextBox() {
                 onChange={(event) => setText(event.target.value)}
                 rows={15}
                 cols={50}
-                className='bg-gray-200 p-8'
+                className='bg-gray-200 p-8 rounded-md'
             />
 
 
             <textarea
-                className='bg-gray-200 p-8 container mx-auto h-150 overflow-y-scroll'
+                className='bg-gray-200 p-8 rounded-md'
                 style={{ whiteSpace: 'pre-line' }}
                 readOnly
                 value={rep}
             />
-            <select value={grade} onChange={handleOptionChange}>
-                {renderOptions()}
-            </select>
+            <div className='grid grid-cols-2 gap-4 p-8'>
+                <select
+                    value={grade}
+                    onChange={handleOptionChange}
+                    className='bg-gray-200 border rounded-sm'
+                >
+                    {renderOptions()}
+                </select>
 
-            <button onClick={handleConfirm}>Confirm</button>
+                <button
+                    className='bg-gray-200 border rounded-sm'
+                    onClick={handleConfirm}
+                >
+                    Confirm
+                </button>
+            </div>
+            <textarea
+                className='bg-gray-200 p-8 rounded-md'
+                
+                readOnly
+                value={terms.map((entry) => {
+                    return `- ${entry[0]}: ${entry[1]}\n`
+                })}
+            />
+            
+
 
         </div>
 
